8c2da8f36936d8549531316b29b548b2ced2ab20
Ebrahim Byagowi <ebrahim@gnu.org>
2023-11-15 23:42:23 +0330

    Add python typings

--- aiosseclient.py
+++ aiosseclient.py
@@ -1,63 +1,27 @@
 import re
 import aiohttp
 import warnings
+from typing import (
+    List,
+    Optional,
+    AsyncGenerator,
+    Final,
+)
 
-async def aiosseclient(url, last_id=None, valid_http_codes=[200,301,307], exit_events=[], timeout_total=None, **kwargs):
-    if 'headers' not in kwargs:
-        kwargs['headers'] = {}
+_sse_line_pattern: Final[re.Pattern[str]] = re.compile('(?P<name>[^:]*):?( ?(?P<value>.*))?')
 
-    # The SSE spec requires making requests with Cache-Control: nocache
-    kwargs['headers']['Cache-Control'] = 'no-cache'
+# Good parts of the below class is adopted from https://github.com/btubbs/sseclient/blob/db38dc6/sseclient.py#L101-L163
+class Event:
+    data: str
+    event: str
+    id: Optional[str]
+    retry: Optional[bool]
 
-    # The 'Accept' header is not required, but explicit > implicit
-    kwargs['headers']['Accept'] = 'text/event-stream'
-    
-    if last_id:
-        kwargs['headers']['Last-Event-ID'] = last_id
-    
-    # Override default timeout of 5 minutes
-    timeout = aiohttp.ClientTimeout(total=timeout_total, connect=None,
-                      sock_connect=None, sock_read=None)
-    async with aiohttp.ClientSession(timeout=timeout) as session:
-        try:
-            yield Event("session created: %s" % session, "stream_telemetry")
-            response = await session.get(url, **kwargs)
-            if response.status not in valid_http_codes:
-                yield Event("Invalid HTTP response.status: %s" % response.status, "stream_failed")
-                await session.close()
-            lines = []
-            async for line in response.content:
-                line = line.decode('utf8')
-
-                if line == '\n' or line == '\r' or line == '\r\n':
-                    if lines[0] == ':ok\n':
-                        lines = []
-                        continue
-
-                    current_event = Event.parse(''.join(lines))
-                    yield current_event
-                    if current_event.event in exit_events:
-                        await session.close()
-                    lines = []
-                else:
-                    lines.append(line)
-        except Exception as sseerr:
-            yield Event("Exception: %s" % sseerr, "session_failed")
-            await session.close()
-
-
-# Below code is directly brought from https://github.com/btubbs/sseclient/blob/db38dc6/sseclient.py#L101-L163
-# Also some bits of aiosseclient() are also brought from the same file
-class Event(object):
-
-    sse_line_pattern = re.compile('(?P<name>[^:]*):?( ?(?P<value>.*))?')
-
-    def __init__(self, data='', event='message', id=None, retry=None):
+    def __init__(self, data: str='', event: str='message', id: Optional[str]=None, retry: Optional[bool]=None):
         self.data = data
         self.event = event
         self.id = id
         self.retry = retry
-
 
     def dump(self):
         lines = []
@@ -76,7 +40,6 @@ class Event(object):
 
     def encode(self):
         return self.dump().encode('utf-8')
-
         
     @classmethod
     def parse(cls, raw):
@@ -86,7 +49,7 @@ class Event(object):
         """
         msg = cls()
         for line in raw.splitlines():
-            m = cls.sse_line_pattern.match(line)
+            m = _sse_line_pattern.match(line)
             if m is None:
                 # Malformed line.  Discard but warn.
                 warnings.warn('Invalid SSE line: "%s"' % line, SyntaxWarning)
@@ -116,4 +79,50 @@ class Event(object):
 
     def __str__(self):
         return self.data
-    
+
+async def aiosseclient(
+    url: str,
+    last_id: Optional[str]=None,
+    valid_http_codes: List[int]=[200, 301, 307],
+    exit_events: List[str]=[],
+    timeout_total: Optional[float]=None,
+    headers: Optional[dict[str, str]]={}
+) -> AsyncGenerator[Event, None]:
+    # The SSE spec requires making requests with Cache-Control: nocache
+    headers['Cache-Control'] = 'no-cache'
+
+    # The 'Accept' header is not required, but explicit > implicit
+    headers['Accept'] = 'text/event-stream'
+
+    if last_id:
+        headers['Last-Event-ID'] = last_id
+
+    # Override default timeout of 5 minutes
+    timeout = aiohttp.ClientTimeout(total=timeout_total, connect=None,
+                                    sock_connect=None, sock_read=None)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        try:
+            yield Event("session created: %s" % session, "stream_telemetry")
+            response = await session.get(url, headers=headers)
+            if response.status not in valid_http_codes:
+                yield Event("Invalid HTTP response.status: %s" % response.status, "stream_failed")
+                await session.close()
+            lines = []
+            async for line in response.content:
+                line = line.decode('utf8')
+
+                if line == '\n' or line == '\r' or line == '\r\n':
+                    if lines[0] == ':ok\n':
+                        lines = []
+                        continue
+
+                    current_event = Event.parse(''.join(lines))
+                    yield current_event
+                    if current_event.event in exit_events:
+                        await session.close()
+                    lines = []
+                else:
+                    lines.append(line)
+        except Exception as sseerr:
+            yield Event("Exception: %s" % sseerr, "session_failed")
+            await session.close()
