ec6d219f5b5906c1356f90ae09a282d10786f540
Ebrahim Byagowi <ebrahim@gnu.org>
2023-11-18 00:15:45 +0330

    Declare Python 3.8 as the minimum supported python
    As we don't have bots for older versions

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.8'
     ]
 )
